6b88a20975715ea88dba0e33e2a0fa7f4b8af4e9
Steven Hicks <steven.hicks@camunda.com>
2024-02-15 11:02:34 -0600

    feat: add client
    Co-authored-by: Christina Ausley <christina.ausley@camunda.com>

--- console.js
+++ console.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { getAccessToken } from "./auth.js";
 
+// Editor's note: nothing below this will exist when they start the tutorial.
+
 // An action that lists all clients.
 async function listClients() {
   // Every request needs an access token.
@@ -34,8 +36,42 @@ async function listClients() {
   );
 }
 
+// An action that adds a new client.
 async function addClient([clientName]) {
-  console.log("adding client:", clientName);
+  // Every request needs an access token.
+  const accessToken = await getAccessToken();
+
+  // These settings come from your .env file.
+  const consoleApiUrl = process.env.CONSOLE_API_URL;
+  const clusterId = process.env.CLUSTER_ID;
+
+  // This is the API endpoint to list all clients within a cluster.
+  const url = `${consoleApiUrl}/clusters/${clusterId}/clients`;
+
+  // Call the clients endpoint.
+  var options = {
+    method: "POST",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
+    },
+    data: {
+      clientName: clientName
+    }
+  };
+  const response = await axios(options);
+
+  // Process the results from the API call.
+  console.log(response.data);
+  const newClient = response.data;
+
+  // Emit clients to output.
+  //  NOTE: In real life, you probably want to capture the
+  //    `clientId` and `clientSecret` properties from the response.
+  console.log(
+    `New client name is ${newClient.name}. Permissions are ${newClient.permissions}.`
+  );
 }
 
 function viewClient([clientId]) {
