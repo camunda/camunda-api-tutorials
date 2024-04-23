b6581391743591f212c1164abd43d2d69bf8edb7
Steven Hicks <steven.hicks@camunda.com>
2024-04-23 11:55:12 -0500

    refactor: getAccessToken can be used by multiple APIs
    Co-authored-by: Christina Ausley <christina.ausley@camunda.com>

--- auth.js
+++ auth.js
@@ -1,34 +1,67 @@
 import { AuthorizationCode } from "simple-oauth2";
 
-// These credentials come from your .env file.
-const clientID = process.env.ADMINISTRATION_CLIENT_ID;
-const clientSecret = process.env.ADMINISTRATION_CLIENT_SECRET;
+const adminConfiguration = {
+  // These credentials come from your .env file.
+  clientID: process.env.ADMINISTRATION_CLIENT_ID,
+  clientSecret: process.env.ADMINISTRATION_CLIENT_SECRET
+};
+
+const componentConfiguration = {
+  // These credentials come from your .env file.
+  clientID: process.env.OPTIMIZE_CLIENT_ID, // TODO: change env variable name
+  clientSecret: process.env.OPTIMIZE_CLIENT_SECRET // TODO: change env variable name
+};
 
 // Configure our authorization request.
-const config = {
-  client: {
-    id: clientID,
-    secret: clientSecret
-  },
-  auth: {
-    // This is the URL for our auth server.
-    tokenHost: "https://login.cloud.camunda.io/oauth/token"
-  },
-  options: {
-    authorizationMethod: "body"
-  }
-};
-const client = new AuthorizationCode(config);
+
+function configureAuthorizationClient(targetApi) {
+  const configSource =
+    targetApi === "administration"
+      ? adminConfiguration
+      : componentConfiguration;
+
+  const config = {
+    client: {
+      id: configSource.clientID,
+      secret: configSource.clientSecret
+    },
+    auth: {
+      // This is the URL for our auth server.
+      tokenHost: "https://login.cloud.camunda.io/oauth/token"
+    },
+    options: {
+      authorizationMethod: "body"
+    }
+  };
+  return new AuthorizationCode(config);
+}
 
 // Define additional parameters for the authorization request.
-const tokenParams = {
-  // This audience is specific to the Camunda API we are calling.
-  audience: "api.cloud.camunda.io"
-};
+function getTokenParams(audience) {
+  return {
+    // This audience is specific to the Camunda API we are calling.
+    audience
+  };
+}
 
 // This function can be used by callers to retrieve a token prior to their API calls.
-export async function getAccessToken() {
+/**
+ *
+ * @param {"administration" | "components"} targetApi Use "administration" for only the administration API; "components" for all other component APIs.
+ * @param {string} audience
+ * @returns
+ */
+export async function getAccessToken(targetApi, audience) {
+  if (targetApi !== "administration" && targetApi !== "components") {
+    throw new Error(
+      "Unexpected targetApi value. Expecing 'administration' or 'components'."
+    );
+  }
+
   try {
+    const client = configureAuthorizationClient(targetApi);
+    const tokenParams = getTokenParams(audience);
+
     const result = await client.getToken(tokenParams);
     const accessToken = client.createToken(result);
     // Return the actual token that can be passed as an Authorization header in each request.
