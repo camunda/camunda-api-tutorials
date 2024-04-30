7c79e93927b7ece4aa1ae3dacc4dd0a966f6b587
Christina Ausley <christina.ausley@camunda.com>
2024-04-30 16:17:12 -0400

    initial draft modeler api

---
+++ modeler.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { getAccessToken } from "./auth.js";
+
+// An action that retrieves a file.
+async function retrieveFile([fileId]) {
+    const modelerAudience = process.env.MODELER_AUDIENCE;
+    const accessToken = await getAccessToken("components", modelerAudience);
+
+    const modelerApiUrl = process.env.MODELER_BASE_URL;
+
+    const url = `${modelerApiUrl}/api/v1/files/${fileId}`;
+
+// Configure the API call.
+  const options = {
+    method: "GET",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
+    }
+  };
+
+  try {
+    const response = await axios(options);
+    const results = response.data;
+
+    results.forEach(x => console.log(`ID: ${x.fileId}`));
+  } catch (error) {
+    // Emit an error from the server.
+    console.error(error.message);
+  }
+}
+
+async function deleteFile([fileId]) {
+    console.log(`deleting file ${fileId}`);
+  
+    const modelerAudience = process.env.MODELER_AUDIENCE;
+    const accessToken = await getAccessToken("components", modelerAudience);
+    const modelerApiUrl = process.env.MODELER_API_URL;
+  
+    const url = `${modelerApiUrl}/api/v1/files/${fileId}`;
+  
+    // Configure the API call.
+    const options = {
+      method: "DELETE",
+      url,
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${accessToken}`
+      }
+    };
+  
+    try {
+      // Call the delete endpoint.
+      const response = await axios(options);
+  
+      // Process the results from the API call.
+      if (response.status === 204) {
+        console.log(`File ${fileId} was deleted!`);
+      } else {
+        // Emit an unexpected error message.
+        console.error("Unable to delete file!");
+      }
+    } catch (error) {
+      // Emit an error from the server.
+      console.error(error.message);
+    }
+  }
+
+export default {
+    retrieve: retrieveFile,
+    delete: deleteFile
+  };
