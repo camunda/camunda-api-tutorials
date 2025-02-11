2012f00ce0716c20f19e6124e052e38058d4dc16
Christina Ausley <christina.ausley@camunda.com>
2025-02-11 16:08:16 -0500

    update js file

--- camunda-8.js
+++ camunda-8.js
@@ -1,24 +1,14 @@
 import axios from "axios";
 import { getAccessToken } from "../auth.js";
 
-async function deployResources([]) {
-      console.log(`Resources deployed.`);
-  }
-
-async function createProcessInstance([]) {
-      console.log("Process instance created!");
-}
-
-async function viewProcessInstance([processInstanceKey]) {
-      console.log("Process instance:", processInstance);
-  }
-
 // These functions are aliased to specific command names for terseness.
 //   The name of each property translates to a method that can be called by the CLI.
-//   e.g. if we export a function named `assign`, you can run `npm run cli zeebe deploy`.
+//   e.g. if we export a function named `assign`, you can run `npm run cli camunda8 get`.
 
 export default {
-  deploy: deployResources,
-  create: createProcessInstance,
-  view: viewProcessInstance
+  get: getUser,
+  create: createGroup,
+  assign: assignUser,
+  retrieve: retrieveGroup,
+  delete: deleteGroup
 };
