373027a3220e8c379c15b9b1ec617e5629d594f8
Christina Ausley <christina.ausley@camunda.com>
2025-02-11 16:05:07 -0500

    update requests

--- completed/camunda-8.js
+++ completed/camunda-8.js
@@ -7,63 +7,17 @@ const authorizationConfiguration = {
   audience: process.env.ZEEBE_AUDIENCE
 };
 
-// An action that deploys one or more resources (processes, decision models, or forms). This is an atomic call, so either all resources are deployed or none of them are.
-// REQUIRES DETAILS
-async function deployResources([]) {
-    // Every request needs an access token.
-    const accessToken = await getAccessToken(authorizationConfiguration);
-
-    // These settings come from your .env file.
-    const zeebeApiUrl = process.env.ZEEBE_BASE_URL;
-
-    // This is the API endpoint to deploy resources.
-    const url = `${zeebeApiUrl}/deployments`;
-
-    // Configure the API call.
-    const options = {
-      method: "POST",
-      url,
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${accessToken}`
-      },
-
-// REQUIRES DETAILS
-      data: {
-        // The body contains information about the deployed resources.
-        tenantId: tenantId
-      }
-    };
-
-    try {
-      // Call the endpoint.
-      const response = await axios(options);
-
-      // Process the results from the API call.
-    if (response.status === 204) {
-      console.log(`Resources deployed.`);
-    } else {
-      // Emit an unexpected error message.
-      console.error("Unable to deploy resources!");
-    }
-    } catch (error) {
-      // Emit an error from the server.
-      console.error(error.message);
-    }
-  }
-
-// An action that creates and starts an instance of the specified process. The process definition to use to create the instance can be specified using its unique key (as returned by deploy resources).
-// REQUIRES DETAILS
-async function createProcessInstance([]) {
+// Retrieves the current authenticated user key.
+async function getUser() {
   const accessToken = await getAccessToken(authorizationConfiguration);
 
-  const zeebeApiUrl = process.env.ZEEBE_BASE_URL;
-
-  const url = `${zeebeApiUrl}/process-instances`;
+  const camundaApiUrl = process.env.ZEEBE_BASE_URL;
+  // This is the API endpoint to retrieve the current authenticated user key.
+  const url = `${camundaApiUrl}/authentication/me`;
 
   // Configure the API call.
   const options = {
-    method: "POST",
+    method: "GET",
     url,
     headers: {
       Accept: "application/json",
@@ -76,11 +30,168 @@ async function createProcessInstance([]) {
     const response = await axios(options);
 
     // Process the results from the API call.
+    const userKey = response.data;
+
+    // Emit the user ID to output.
+    console.log("User key:", userKey);
+  } catch (error) {
+    // Emit an error from the server.
+    console.error(error.message);
+  }
+}
+
+// An action that creates a group.
+async function createGroup([groupName]) {
+  // Every request needs an access token.
+  const accessToken = await getAccessToken(authorizationConfiguration);
+
+  // These settings come from your .env file.
+  const camundaApiUrl = process.env.ZEEBE_BASE_URL;
+
+  // This is the API endpoint to add a new client to a cluster.
+  const url = `${camundaApiUrl}/groups`;
+
+  // Configure the API call.
+  const options = {
+    method: "POST",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
+    },
+    data: {
+      // The body contains information about the new group.
+      groupName : groupName
+    }
+  };
+
+  try {
+    const response = await axios(options);
+
+    // Process the results from the API call.
+    const newGroup = response.data;
+
+    // Emit new group to output.
+    console.log(
+      `Group added! Name: ${newGroup.name}. Key: ${newGroup.groupKey}.`
+    );
+  } catch (error) {
+    // Emit an error from the server.
+    console.error(error.message);
+  }
+}
+
+// An action that assigns a user to a group by a key.
+async function assignUser([groupKey, userKey]) {
+  // Every request needs an access token.
+  const accessToken = await getAccessToken(authorizationConfiguration);
+
+  // These settings come from your .env file.
+  const camundaApiUrl = process.env.ZEEBE_BASE_URL;
+
+  // This is the API endpoint to assign a user to a group.
+  const url = `${camundaApiUrl}/groups/${groupKey}/users/${userKey}`;
+
+  // Configure the API call.
+  const options = {
+    method: "POST",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
+    },
+    data: {
+      // The body contains information about the new assignment.
+      assignee: assignee
+// Is this right??
+    }
+  };
+
+  try {
+    // Call the add endpoint.
+    const response = await axios(options);
+
+    // Process the results from the API call.
+  if (response.status === 204) {
+    console.log(`Group assigned to ${assignee}.`);
+
+  // Should this be user key above??
+  } else {
+    // Emit an unexpected error message.
+    console.error("Unable to assign this user!");
+  }
+  } catch (error) {
+    // Emit an error from the server.
+    console.error(error.message);
+  }
+}
+
+// An action that retrieves assigned member keys within a group.
+async function retrieveGroup([groupKey]) {
+  // Every request needs an access token.
+  const accessToken = await getAccessToken(authorizationConfiguration);
+
+  // These settings come from your .env file.
+  const camundaApiUrl = process.env.ZEEBE_BASE_URL;
+
+  // This is the API endpoint to list all assigned member keys within a group.
+  const url = `${camundaApiUrl}/groups/${groupKey}`;
+
+  // Configure the API call.
+  const options = {
+    method: "GET",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
+    }
+  };
+
+  try {
+    // Call the endpoint.
+    const response = await axios(options);
+
+    // Process the results from the API call.
+    const results = response.data;
+
+    // Emit clients to output.
+    results.forEach(x => console.log(`Name: ${x.name}; ID: ${x.assignedMemberKeys
+    }`));
+// Not sure how to format the above -- is forEach needed??
+  } catch (error) {
+    // Emit an error from the server.
+    console.error(error.message);
+  }
+}
+
+// An action to delete a group.
+async function deleteGroup([groupKey]) {
+  const accessToken = await getAccessToken(authorizationConfiguration);
+
+  const camundaApiUrl = process.env.ZEEBE_BASE_URL;
+
+  const url = `${camundaApiUrl}/groups/${groupKey}`;
+
+  // Configure the API call.
+  const options = {
+    method: "DELETE",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
+    }
+  };
+
+  try {
+    // Call the delete endpoint.
+    const response = await axios(options);
+
+    // Process the results from the API call.
     if (response.status === 204) {
-      console.log("Process instance created!");
+      console.log("Group deleted!");
     } else {
       // Emit an unexpected error message.
-      console.error("Unable to create process instance!");
+      console.error("Unable to delete this group!");
     }
   } catch (error) {
     // Emit an error from the server.
@@ -88,47 +199,14 @@ async function createProcessInstance([]) {
   }
 }
 
-// An action to get the process instance by the process instance key.
-async function viewProcessInstance([processInstanceKey]) {
-    // Every request needs an access token.
-    const accessToken = await getAccessToken(authorizationConfiguration);
-  
-    const zeebeApiUrl = process.env.ZEEBE_BASE_URL;
-  
-    const url = `${zeebeApiUrl}/process-instances/${processInstanceKey}`;
-  
-    // Configure the API call.
-    const options = {
-      method: "GET",
-      url,
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${accessToken}`
-      }
-    };
-  
-    // REQUIRES DETAILS
-    try {
-      // Call the endpoint.
-      const response = await axios(options);
-  
-      // Process the results from the API call.
-      const  = response.data;
-  
-      // Emit the project to output.
-      console.log("Process instance:", processInstance);
-    } catch (error) {
-      // Emit an error from the server.
-      console.error(error.message);
-    }
-  }
-
 // These functions are aliased to specific command names for terseness.
 //   The name of each property translates to a method that can be called by the CLI.
-//   e.g. if we export a function named `deploy`, you can run `npm run cli camunda8 deploy`.
+//   e.g. if we export a function named `list`, you can run `npm run cli zeebe get`.
 
 export default {
-  deploy: deployResources,
-  create: createProcessInstance,
-  view: viewProcessInstance
+  get: getUser,
+  create: createGroup,
+  assign: assignUser,
+  retrieve: retrieveGroup,
+  delete: deleteGroup
 };
