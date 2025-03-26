f12e8ac92c2684287c5838360cbc8052706ec957
Christina Ausley <christina.ausley@camunda.com>
2025-03-26 17:43:11 -0400

    intermediate c8 tutorial

---
+++ camunda-8-intermediate.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { getAccessToken } from "./auth.js";
+
+// An action that deploys one or more resources (e.g. processes, decision models, or forms). This is an atomic call, i.e. either all resources are deployed or none of them are.
+async function deployResources() {
+  console.log("deploying resources");
+}
+
+// An action that creates and starts an instance of the specified process.
+async function createInstance() {
+  console.log(`creating process instance`);
+}
+
+// These functions are aliased to specific command names for terseness.
+//   The name of each property translates to a method that can be called by the CLI.
+//   e.g. if we export a function named `list`, you can run `npm run cli zeebe get`.
+
+export default {
+  deploy: deployResources,
+  create: createInstance
+};
