e9dc74161fd8813ce80db4bd17580153b105c97b
Christina Ausley <christina.ausley@camunda.com>
2025-04-15 11:44:50 -0400

    outline functions

--- completed/camunda-process-instances.js
+++ completed/camunda-process-instances.js
@@ -3,13 +3,126 @@ import { getAccessToken } from "../auth.js";
 
 // An action that deploys one or more resources (e.g. processes, decision models, or forms). This is an atomic call, i.e. either all resources are deployed or none of them are.
 async function deployResources() {
-  console.log("deploying resources");
+
+    // Every request needs an access token.
+    const accessToken = await getAccessToken(authorizationConfiguration);
+
+    // These settings come from your .env file.
+    const camundaApiUrl = process.env.CAMUNDA_REST_ADDRESS;
+
+    // This is the API endpoint.
+    const url = `${camundaApiUrl}/deployments`;
+
+  // Configure the API call.
+  const options = {
+    method: "POST",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
+    },
+    data: {
+      // The body contains information about the new role -- resources and tenantId?
+    }
+  };
+
+  try {
+    const response = await axios(options);
+
+    // Emit new role to output.
+    console.log(`deploying resources`);
+  } catch (error) {
+    // Emit an error from the server.
+    console.error(error.message);
+  }
 }
+
+```mynotes
+------myboundary
+Content-Disposition: form-data; name="tenantId"
+Content-Type: text/plain
+myCustomTenant
+------myboundary
+Content-Disposition: form-data; name="resource"; filename="file1.txt"
+Content-Type: text/plain
+[file content goes there]
+------myboundary
+Content-Disposition: form-data; name="resource"; filename="file2.png"
+Content-Type: image/png
+[file content goes there]
+------myboundary
+
+var https = require('follow-redirects').https;
+var fs = require('fs');
+
+var options = {
+  'method': 'POST',
+  'hostname': 'localhost',
+  'port': 8080,
+  'path': '/v2/deployments',
+  'headers': {
+    'Content-Type': 'multipart/form-data',
+    'Accept': 'application/json'
+  },
+  'maxRedirects': 20
+};
+
+var req = https.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+
+  res.on("error", function (error) {
+    console.error(error);
+  });
+});
+
+
+```
 
 // An action that creates and starts an instance of the specified process.
 async function createInstance() {
-  console.log(`creating process instance`);
+
+      // Every request needs an access token.
+      const accessToken = await getAccessToken(authorizationConfiguration);
+
+      // These settings come from your .env file.
+      const camundaApiUrl = process.env.CAMUNDA_REST_ADDRESS;
+
+      // This is the API endpoint.
+      const url = `${camundaApiUrl}/process-instances`;
+
+  // Configure the API call.
+  const options = {
+    method: "POST",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
+    },
+    data: {
+      // The body contains information about the new role -- what do I need to include?
+    }
+  };
+
+  try {
+    const response = await axios(options);
+
+    // Emit new role to output.
+    console.log(`creating process instance`);
+  } catch (error) {
+    // Emit an error from the server.
+    console.error(error.message);
+  }
 }
+
 
 // An action that gets the process instance by the process instance key.
 async function viewInstance([processInstanceKey]) {
