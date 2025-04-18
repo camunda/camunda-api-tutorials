7134cc051e72fdd5ca21fb08db0091e006bf0eae
Steven Hicks <steven.hicks@camunda.com>
2025-04-18 11:45:15 -0500

    make process instances work
    Co-authored-by: Christina Ausley <christina.ausley@camunda.com>

--- completed/camunda-process-instances.js
+++ completed/camunda-process-instances.js
@@ -1,17 +1,35 @@
+import path from "path";
+import fs from "fs";
 import axios from "axios";
+import FormData from "form-data";
 import { getAccessToken } from "../auth.js";
+
+const authorizationConfiguration = {
+  clientId: process.env.CAMUNDA_CLIENT_ID,
+  clientSecret: process.env.CAMUNDA_CLIENT_SECRET,
+  // These settings come from your .env file. Note that CAMUNDA_TOKEN_AUDIENCE is represented by ZEEBE_TOKEN_AUDIENCE in the Console UI.
+  audience: process.env.CAMUNDA_TOKEN_AUDIENCE
+};
 
 // An action that deploys one or more resources (e.g. processes, decision models, or forms). This is an atomic call, i.e. either all resources are deployed or none of them are.
 async function deployResources() {
+  // Every request needs an access token.
+  const accessToken = await getAccessToken(authorizationConfiguration);
 
-    // Every request needs an access token.
-    const accessToken = await getAccessToken(authorizationConfiguration);
+  // These settings come from your .env file.
+  const camundaApiUrl = process.env.CAMUNDA_REST_ADDRESS;
 
-    // These settings come from your .env file.
-    const camundaApiUrl = process.env.CAMUNDA_REST_ADDRESS;
+  // This is the API endpoint.
+  const url = `${camundaApiUrl}/deployments`;
 
-    // This is the API endpoint.
-    const url = `${camundaApiUrl}/deployments`;
+  const formData = new FormData();
+  // Read the BPMN file and add it to the form data
+  const bpmnFilePath = path.resolve("resources/calculate-sales-tax.bpmn");
+  const fileContent = fs.readFileSync(bpmnFilePath);
+  formData.append("resources", fileContent, {
+    filename: "calculate-sales-tax.bpmn",
+    contentType: "application/xml"
+  });
 
   // Configure the API call.
   const options = {
@@ -19,85 +37,38 @@ async function deployResources() {
     url,
     headers: {
       Accept: "application/json",
-      Authorization: `Bearer ${accessToken}`
+      Authorization: `Bearer ${accessToken}`,
+      ...formData.getHeaders()
     },
-    data: {
-      // The body contains information about the new role -- resources and tenantId?
-    }
+    data: formData
   };
 
   try {
     const response = await axios(options);
+    const deployedResources = response.data.deployments;
 
-    // Emit new role to output.
-    console.log(`deploying resources`);
+    // Emit deployed resources
+    deployedResources.forEach(x =>
+      console.log(
+        `Process Definition Key: ${x.processDefinition.processDefinitionKey}; Process Definition Id: ${x.processDefinition.processDefinitionId}`
+      )
+    );
   } catch (error) {
     // Emit an error from the server.
     console.error(error.message);
   }
 }
-
-```mynotes
-------myboundary
-Content-Disposition: form-data; name="tenantId"
-Content-Type: text/plain
-myCustomTenant
-------myboundary
-Content-Disposition: form-data; name="resource"; filename="file1.txt"
-Content-Type: text/plain
-[file content goes there]
-------myboundary
-Content-Disposition: form-data; name="resource"; filename="file2.png"
-Content-Type: image/png
-[file content goes there]
-------myboundary
-
-var https = require('follow-redirects').https;
-var fs = require('fs');
-
-var options = {
-  'method': 'POST',
-  'hostname': 'localhost',
-  'port': 8080,
-  'path': '/v2/deployments',
-  'headers': {
-    'Content-Type': 'multipart/form-data',
-    'Accept': 'application/json'
-  },
-  'maxRedirects': 20
-};
-
-var req = https.request(options, function (res) {
-  var chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function (chunk) {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-
-  res.on("error", function (error) {
-    console.error(error);
-  });
-});
-
-
-```
 
 // An action that creates and starts an instance of the specified process.
-async function createInstance() {
+async function createInstance([processDefinitionKey]) {
+  // Every request needs an access token.
+  const accessToken = await getAccessToken(authorizationConfiguration);
 
-      // Every request needs an access token.
-      const accessToken = await getAccessToken(authorizationConfiguration);
+  // These settings come from your .env file.
+  const camundaApiUrl = process.env.CAMUNDA_REST_ADDRESS;
 
-      // These settings come from your .env file.
-      const camundaApiUrl = process.env.CAMUNDA_REST_ADDRESS;
-
-      // This is the API endpoint.
-      const url = `${camundaApiUrl}/process-instances`;
+  // This is the API endpoint.
+  const url = `${camundaApiUrl}/process-instances`;
 
   // Configure the API call.
   const options = {
@@ -108,25 +79,27 @@ async function createInstance() {
       Authorization: `Bearer ${accessToken}`
     },
     data: {
-      // The body contains information about the new role -- what do I need to include?
+      processDefinitionKey: processDefinitionKey,
+      variables: {
+        total: 90.0
+      }
     }
   };
 
   try {
     const response = await axios(options);
+    const processInstance = response.data;
 
     // Emit new role to output.
-    console.log(`creating process instance`);
+    console.log(`Process Instance Key: ${processInstance.processInstanceKey}`);
   } catch (error) {
     // Emit an error from the server.
     console.error(error.message);
   }
 }
-
 
 // An action that gets the process instance by the process instance key.
 async function viewInstance([processInstanceKey]) {
-
   // Every request needs an access token.
   const accessToken = await getAccessToken(authorizationConfiguration);
 
@@ -134,36 +107,33 @@ async function viewInstance([processInstanceKey]) {
   const camundaApiUrl = process.env.CAMUNDA_REST_ADDRESS;
 
   // This is the API endpoint to get a specific role.
-  const url = `${camundaApiUrl}/processinstances/${processInstanceKey}`;
+  const url = `${camundaApiUrl}/process-instances/${processInstanceKey}`;
 
-    // Configure the API call.
-    const options = {
-      method: "GET",
-      url,
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${accessToken}`
-      }
-    };
-
-    try {
-      // Call the endpoint.
-      const response = await axios(options);
-  
-      // Process the results from the API call.
-      const results = response.data;
-  
-      // Emit role to output.
-      console.log(
-        `Process instance name: ${results.processDefinitionName
-        }; State: ${
-          results.state
-        };`
-      );
-    } catch (error) {
-      // Emit an error from the server.
-      console.error(error.message);
+  // Configure the API call.
+  const options = {
+    method: "GET",
+    url,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`
     }
+  };
+
+  try {
+    // Call the endpoint.
+    const response = await axios(options);
+
+    // Process the results from the API call.
+    const results = response.data;
+
+    // Emit role to output.
+    console.log(
+      `Process instance name: ${results.processDefinitionName}; State: ${results.state};`
+    );
+  } catch (error) {
+    // Emit an error from the server.
+    console.error(error.message);
+  }
 }
 
 // These functions are aliased to specific command names for terseness.
